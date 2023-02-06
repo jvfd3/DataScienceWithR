a805800c25c62d5ba8df7d9a74d59961c2c3ae23
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2023-02-05 21:05:56 -0300

    feat: Big Bang
    Putting all content inside the main folder

---
+++ Old/teste3.r
@@ -0,0 +1,10 @@
+install.packages("tidyverse")
+install.packages("palmerpenguins")
+install.packages("nycflights13")
+library(tidyverse)
+library(palmerpenguins)
+library(nycflights13)
+nycflights13::flights
+
+
+flights
